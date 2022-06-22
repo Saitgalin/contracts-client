d72a9304e60f6d3489806ee9ee7f5b7667ca4788
aigizsa <saitgalin18@gmail.com>
2022-06-22 20:37:34 +0500

    clear unused imports

--- src/main/java/com/test/contractsclient/HelloApplication.java
+++ src/main/java/com/test/contractsclient/HelloApplication.java
@@ -1,14 +1,8 @@
 package com.test.contractsclient;
 
 import javafx.application.Application;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
 import java.io.IOException;
